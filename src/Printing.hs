bc334a96d6abf9e4eee0fa25a0cbf0c5d3e51dfa
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-11-01 14:55:56 +0000

    Style changes in printing and documentation

--- src/Printing.hs
+++ src/Printing.hs
@@ -1,23 +1,28 @@
-module Printing (
-    Pattern, instantiatePattern,
-    PrintingData(..)
-) where
+module Printing
+  ( Pattern
+  , instantiatePattern
+  , PrintingData(..)
+  ) where
 
 import Data.Map (Map)
 
 ----------------------------------------------------------------------------------------------------
 
+-- | Patterns are strings, with % used to mark holes.
 type Pattern = String
 
+-- | Instantiate a pattern by putting the given strings in holes, in order.
 instantiatePattern :: Pattern -> [String] -> String
 instantiatePattern ('%':cs) (s:ss) = s ++ instantiatePattern cs ss
-instantiatePattern (c:cs) ss = c : instantiatePattern cs ss
-instantiatePattern [] [] = ""
+instantiatePattern (c:cs)   ss     = c : instantiatePattern cs ss
+instantiatePattern []       []     = ""
+instantiatePattern _        _      = error "instantiate pattern error"
 
+-- | Describes how to turn terms into TeX.
 data PrintingData = PrintingData {
-    termPatterns :: Map String Pattern,
-    formulaPatterns :: Map String Pattern,
-    nounPatterns :: Map String Pattern,
+    termPatterns      :: Map String Pattern,
+    formulaPatterns   :: Map String Pattern,
+    nounPatterns      :: Map String Pattern,
     adjectivePatterns :: Map String Pattern
-  }                                                                                    deriving Show
-
+  }
+  deriving Show
